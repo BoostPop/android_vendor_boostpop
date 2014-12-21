55c4fe7d7c14b61305f1ca586f8ea86d3e78e13c
Crostantin <crostantin@gmail.com>
2014-12-21 21:51:18 +0100

    Update to LRX22G

--- products/boostpop_hammerhead.mk
+++ products/boostpop_hammerhead.mk
@@ -13,6 +13,6 @@ PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" 
-BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys" 
-PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
+BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0.2/LRX22G/1602158:user/release-keys" 
+PRIVATE_BUILD_DESC="hammerhead-user 5.0.2 LRX22G 1602158 release-keys"
 
