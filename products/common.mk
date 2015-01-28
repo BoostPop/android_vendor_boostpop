3cb6d7a5a36542ad9a243f4f6a98b8024cfcb27c
Crostantin <crostantin@gmail.com>
2015-01-28 14:28:15 +0100

    Enable tethering for all devices
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- products/common.mk
+++ products/common.mk
@@ -27,6 +27,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
+    net.tethering.noprovisioning=true \
     keyguard.no_require_sim=true
 
 # Boost build prop
