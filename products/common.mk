2eee5507e44942575b160505edf370b4d8ad9688
Crostantin <crostantin@gmail.com>
2015-02-26 20:16:49 +0100

    Improved versioning code
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- products/common.mk
+++ products/common.mk
@@ -6,14 +6,16 @@ PRODUCT_DEVICE := generic
 # Version info
 ROM_VERSION_MAJOR := 2
 ROM_VERSION_MINOR := 9
+ROM_VERSION_BETA := false
+
+# Block based ota flag default to off to get old style ota zip back (To get back block based zip, just enable to true.)
+TARGET_USES_BLOCK_BASED_OTA := false
 
 VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)
 
-# Block based ota flag default to off to get old style ota zip back (To get back block based zip, just enable to true.)
-TARGET_USES_BLOCK_BASED_OTA := true
-
 # Boostpop release are no block base. We use block for beta testing.
-ifeq ($(TARGET_USES_BLOCK_BASED_OTA),true)
+ifeq ($(ROM_VERSION_BETA),true)
+    TARGET_USES_BLOCK_BASED_OTA := true
     VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)-beta
 endif
 
