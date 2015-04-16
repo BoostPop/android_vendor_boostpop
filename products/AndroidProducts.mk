3b53c6210e8287f9a027a07d73da90a29fa8a5e5
Paul Beeler <pbeeler80@gmail.com>
2015-04-16 03:53:15 -0600

    Move shell unset variables outside of device makefiles
    Change-Id: I49e7c8325de7fa78ea5462e67eeaf4999d184532
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,7 @@
+$(shell unset EXTRA_SABERMOD_GCC_CFLAGS)
+$(shell unset GRAPHITE_KERNEL_FLAGS)
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/boostpop_hammerhead.mk \
     $(LOCAL_DIR)/boostpop_mako.mk \
+
