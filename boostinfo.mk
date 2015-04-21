7190eeabec331f3038c740467a0d5ee3572deff4
Crostantin <crostantin@gmail.com>
2015-04-21 18:41:56 +0200

    Allow smart versioning
    based on AOSPA and OMNI original work
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

---
+++ boostinfo.mk
@@ -0,0 +1,13 @@
+# Export rom name
+export VENDOR := boost
+
+# Version info
+ROM_VERSION_MAJOR := 3
+ROM_VERSION_MINOR := 1
+ROM_VERSION_RELEASE := true
+
+# Block based ota flag default to off to get old style ota zip back (To get back block based zip, just enable to true.)
+TARGET_USES_BLOCK_BASED_OTA := true 
+
+# Use deoxed build for debug
+TARGET_USES_DEOXPREOT_OTA := false
