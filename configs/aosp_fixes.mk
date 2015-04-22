a0b774e937480602833a6c5f1061fe1051096435
Crostantin <crostantin@gmail.com>
2015-04-23 01:08:31 +0200

    add facelock build.prop stuff in
    credits to @Sykopompos
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -11,6 +11,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
 
 # Proprietary latinime lib needed for swyping
 PRODUCT_COPY_FILES += \
