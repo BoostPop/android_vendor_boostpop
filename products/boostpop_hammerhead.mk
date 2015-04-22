a159763ebc2c3a3c15e3e73cb8e142aa43174b5b
Crostantin <crostantin@gmail.com>
2015-04-22 09:48:36 +0200

    Update to LMY47V
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- products/boostpop_hammerhead.mk
+++ products/boostpop_hammerhead.mk
@@ -16,6 +16,6 @@ PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" 
-BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.1/LMY47O/1767468:user/release-keys" 
-PRIVATE_BUILD_DESC="hammerhead-user 5.1 LMY47O 1767468 release-keys"
+BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.1.1/LMY47V/1767468:user/release-keys" 
+PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY47V 1767468 release-keys"
 
