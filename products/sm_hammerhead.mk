49471b538fcfb22185b592712a0363c5e204ac27
Paul Beeler <pbeeler80@gmail.com>
2015-04-25 00:17:36 -0600

    Make SaberMod ARM Mode enabled by default and use 4.9 for hammerhead:
    Now that bluetooth is fixed when SaberMod ARM Mode is enabled, I
    see no reason why this shouldn't be default.  SaberMod ARM Mode also
    fixes other bugs, that allow gcc 4.9 for the ROM to function with force
    closes of setup wizard.
    
    Change-Id: I6ac2e9d0f5371a95b13786ab01989096a0da351b
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	products/sm_hammerhead.mk

--- products/sm_hammerhead.mk
+++ products/sm_hammerhead.mk
@@ -25,11 +25,10 @@ ifeq ($(strip $(HOST_OS)),linux)
 
   # Sabermod configs
   TARGET_ARCH := arm
-  TARGET_SM_AND := 4.8
+  TARGET_SM_AND := 4.9
   TARGET_SM_KERNEL := 6.0
   HAMMERHEAD_THREADS := 4
   PRODUCT_THREADS := $(HAMMERHEAD_THREADS)
-  ENABLE_SABERMOD_ARM_MODE := true
 
   GRAPHITE_KERNEL_FLAGS := \
     -floop-parallelize-all \
