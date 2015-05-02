73dcc13123e324b99318fd758dad8c3c5bbe1e0f
Crostantin <crostantin@gmail.com>
2015-05-02 07:21:41 +0200

    BoostPop 3.2 and fix version.mk
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- configs/version.mk
+++ configs/version.mk
@@ -1,16 +1,16 @@
 # Boostpop release are not block based and comes with the pre-odex system apk. 
 # We use other settings for for beta testing.
 
-ifeq ($(ROM_VERSION_RELEASE),true)
-    TARGET_USES_BLOCK_BASED_OTA := true
-    TARGET_USES_DEOXPREOT_OTA := true
-    VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)
-else
 ifeq ($(ROM_VERSION_TAG), )
     VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)-$(shell date -u +%Y_%m_%d)
 else
     VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)-$(ROM_VERSION_TAG)
 endif
+
+ifeq ($(ROM_VERSION_RELEASE),true)
+    TARGET_USES_BLOCK_BASED_OTA := true
+    TARGET_USES_DEOXPREOT_OTA := true
+    VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)
 endif
 
 export ROM_VERSION := $(VERSION)
