ea3a7964939efdfcb2a584e79e6f28ad940a105d
Crostantin <crostantin@gmail.com>
2015-05-04 10:53:31 +0200

    Fix mako
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- products/boostpop_mako.mk
+++ products/boostpop_mako.mk
@@ -5,7 +5,7 @@ $(call inherit-product, device/lge/mako/full_mako.mk)
 include vendor/boostpop/products/sm_mako.mk
 
 # Inherit common product files
-$(call inherit-product, vendor/boostpop/configs/common.mk)
+$(call inherit-product, vendor/boostpop/boostpop_main.mk)
 
 # Setup device specific product configuration
 PRODUCT_NAME := boostpop_mako
