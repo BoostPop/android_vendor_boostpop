725a0c34df0b80fee9178f15178417ac025a355c
Crostantin <crostantin@gmail.com>
2015-05-27 19:49:54 +0200

    Revert SaberMod old stuff
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,4 @@
 
-include vendor/boostpop/configs/sm_clear_vars.mk
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/boostpop_hammerhead.mk \
     $(LOCAL_DIR)/boostpop_mako.mk \
--- products/boostpop_mako.mk
+++ products/boostpop_mako.mk
@@ -15,9 +15,6 @@
 # Inherit AOSP device configuration for mako
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
-# Inherit sabermod device configuration
-include vendor/boostpop/products/sm_mako.mk
-
 # Inherit common product files
 $(call inherit-product, vendor/boostpop/boostpop_main.mk)
 
